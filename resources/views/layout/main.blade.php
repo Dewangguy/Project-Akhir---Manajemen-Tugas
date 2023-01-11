435a80b29481b183ce40aca7282ef7ffab0bb39c
dewangga <dewangguy@gmail.com>
2023-01-11 13:55:02 +0700

    angga

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -549,30 +549,111 @@
                         </div> --}}
                       </div>
                     </div> 
-                    <div class="row">
-                      <div class="col-lg-4 d-flex flex-column">
-                        <div class="row flex-grow">
-                          <div class="col-10 col-lg-4 col-lg-12  ">
-                            <div class="card card-rounded">
-                              <div class="card-body">
-                                <div class="d-sm-flex justify-content-between align-items-start">
-                                  <div>
-                                   <h4 class="card-title card-title-dash">Mengerjakan LKS</h4>
-                                   <h5 class="card-subtitle card-subtitle-dash">Pendidikan Agama Islam</h5>
+                    <tr>
+                      <td>
+                      
+                      <div class="row">
+                        <div class="col-lg-4 d-flex flex-column">
+                          <div class="row flex-grow">
+                            <div class="col-10 col-lg-4 col-lg-12  ">
+                              <div class="card card-rounded">
+                                <div class="card-body">
+                                  <div class="d-sm-flex justify-content-between align-items-start">
+                                    <div>
+                                      <h4 class="card-title card-title-dash">Mengerjakan LKS</h4>
+                                      <h5 class="card-subtitle card-subtitle-dash">Pendidikan Agama Islam</h5>
+                                    </div>
+                                    <div></div>
                                   </div>
-                                  <div></div>
-                                </div>
-                                <div>
-                                  <canvas></canvas>
+                                  <div>
+                                    <canvas></canvas>
+                                  </div>
                                 </div>
                               </div>
                             </div>
                           </div>
                         </div>
-                      </div>
-                      {{-- <div class="col-lg-4 d-flex flex-column">
-                        <div class="row flex-grow">
-                          <div class="col-md-6 col-lg-12 grid-margin stretch-card">
+                        </td>
+                        <td>
+                          <div class="row">
+                            <div class="col-lg-4 d-flex flex-column">
+                              <div class="row flex-grow">
+                                <div class="col-10 col-lg-4 col-lg-12  ">
+                                  <div class="card card-rounded">
+                                    <div class="card-body">
+                                      <div class="d-sm-flex justify-content-between align-items-start">
+                                        <div>
+                                         <h4 class="card-title card-title-dash">Mengerjakan LKS</h4>
+                                         <h5 class="card-subtitle card-subtitle-dash">Pendidikan Agama Islam</h5>
+                                        </div>
+                                        <div></div>
+                                      </div>
+                                      <div>
+                                        <canvas></canvas>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                            {{-- <div class="col-lg-4 d-flex flex-column">
+                              <div class="row flex-grow">
+                                <div class="col-md-6 col-lg-12 grid-margin stretch-card">
+                                  <div class="card bg-primary card-rounded">
+                                    <div class="card-body pb-0">
+                                      <h4 class="card-title card-title-dash text-white mb-4">Status Summary</h4>
+                                      <div class="row">
+                                        <div class="col-sm-4">
+                                          <p class="status-summary-ight-white mb-1">Closed Value</p>
+                                          <h2 class="text-info">357</h2>
+                                        </div>
+                                        <div class="col-sm-8">
+                                          <div class="status-summary-chart-wrapper pb-4">
+                                            <canvas id="status-summary"></canvas>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="col-md-6 col-lg-12 grid-margin stretch-card">
+                                  <div class="card card-rounded">
+                                    <div class="card-body">
+                                      <div class="row">
+                                        <div class="col-sm-6">
+                                          <div class="d-flex justify-content-between align-items-center mb-2 mb-sm-0">
+                                            <div class="circle-progress-width">
+                                              <div id="totalVisitors" class="progressbar-js-circle pr-2"></div>
+                                            </div>
+                                            <div>
+                                              <p class="text-small mb-2">Total Visitors</p>
+                                              <h4 class="mb-0 fw-bold">26.80%</h4>
+                                            </div>
+                                          </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                          <div class="d-flex justify-content-between align-items-center">
+                                            <div class="circle-progress-width">
+                                              <div id="visitperday" class="progressbar-js-circle pr-2"></div>
+                                            </div>
+                                            <div>
+                                              <p class="text-small mb-2">Visits per day</p>
+                                              <h4 class="mb-0 fw-bold">9065</h4>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div> --}}
+                          </div>
+                        </td>
+                      </tr>
+                        {{-- <div class="col-lg-4 d-flex flex-column">
+                          <div class="row flex-grow">
+                            <div class="col-md-6 col-lg-12 grid-margin stretch-card">
                             <div class="card bg-primary card-rounded">
                               <div class="card-body pb-0">
                                 <h4 class="card-title card-title-dash text-white mb-4">Status Summary</h4>
@@ -623,6 +704,7 @@
                         </div>
                       </div> --}}
                     </div>
+                    
                     {{-- <div class="row">
                       <div class="col-lg-8 d-flex flex-column">
                         <div class="row flex-grow">
